2149fb037e614f20b9c7ed2b53cf771fceaf2f0f
Firdavs375 <firdavss.3755@gmail.com>
2023-04-17 16:01:44 +0500

    fix

---
+++ index.js
@@ -0,0 +1,90 @@
+
+//  1.String.
+//  2.Number.
+//  3.Bigint.
+//  4.Boolean.
+//  5.Undefined.
+//  6.Null.
+//  7.Symbol.
+//  8.Object.
+
+
+
+
+//  1.string
+//  2.number
+//  3.boolean
+//  4.null
+//  5.undefined
+//  6.symbol
+//  7.bigint
+
+
+
+
+// for(let i = 1; i <=10; i++){
+//   if(i % 2 == 0){
+//        console.log(i +" Juft son");
+//   }else{
+//        console.log(i +" Toq son");
+//   }
+// }
+
+
+
+// let a = 1
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(a += i);
+// }
+
+
+
+// let a = 1
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(a *= i);
+// }
+
+
+// let Juft = 0
+// let Toq = 0
+
+// for(let i = 1; i <= 10; i++){
+//     if(i % 2 == 0){
+//        console.log(Juft += i);
+//     }else{
+//        console.log(Toq += i);
+//     }
+// }
+
+
+// function MyFunc(a, b){
+//    if(a - b % 2){
+//       return true
+//    }else{
+//       return false
+//    }
+// }
+
+// console.log(MyFunc(10, 0));
+
+
+
+// function MyFunc(a, b){
+//    if(a / b % 1 ){
+//       return false
+//    }else{
+//       return true
+//    }
+// }
+
+// console.log(MyFunc(4,1));
+
+
+// function MyFunc(str1){
+//    for(let i = 0; i < str1.length; i++){
+//        console.log(str1[i])
+//    }
+// }
+// console.log(MyFunc("Erlan"));
---
+++ intex.js
@@ -0,0 +1,174 @@
+//19  [Sum of Polygon Angles](https://edabit.com/challenge/fBJyQSe5Jmbm9hPAG)
+function sumPolygon(n) {
+    return (n-2) * 180
+  }
+  //20 [Basic Variable Assignment](https://edabit.com/challenge/ZNwHGgHvsdnYwJ5WK)
+
+  function nameString(name){
+	var b = "Edabit"
+	var result = name + b
+  	return result
+}
+
+  //21 [The Farm Problem](https://edabit.com/challenge/8Qg78sf5SNDEANKti)
+
+  function animals(chickens, cows, pigs) {
+	chickens *= 2;
+	cows *= 4;
+	pigs *= 4;
+	return chickens + cows + pigs;
+}
+
+  //22 [Are the Numbers Equal?](https://edabit.com/challenge/QSnaSH5S3oxZkwcNc)
+
+  function isSameNum(num1, num2) {
+    return num1 === num2
+  }
+  
+  //23 [Convert Hours and Minutes into Seconds](https://edabit.com/challenge/JesaFi5ntBEbGT8bu)
+
+  function convert(hours, minutes) {
+    return ((hours*60) + minutes) * 60
+  }
+
+  //24 [Equality Check](https://edabit.com/challenge/BGvTMfwxYDRbtaTJ3)
+
+  function checkEquality(a, b) {
+    return a === b;
+  }
+
+  //25 [Sum of Polygon Angles](https://edabit.com/challenge/fBJyQSe5Jmbm9hPAG)
+
+  function sumPolygon(n){
+  	return (n-2) * 180;
+}
+
+  //26 [Basic Variable Assignment](https://edabit.com/challenge/ZNwHGgHvsdnYwJ5WK)
+
+  function nameString(name){
+  	return name+"Edabit"
+  }
+
+  //29 [Convert Hours and Minutes into Seconds](https://edabit.com/challenge/JesaFi5ntBEbGT8bu)
+
+  function convert(hours, minutes) {
+    return ((hours*60) + minutes) * 60
+  }
+
+  //30 [Equality Check](https://edabit.com/challenge/BGvTMfwxYDRbtaTJ3)
+
+  function checkEquality(a, b) {
+    if (a === b) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  //31 [Profitable Gamble](https://edabit.com/challenge/ghbHrRnRiDz9fvQNF)
+  
+  function profitableGamble(prob, prize, pay) {
+    return (prob * prize - pay) > 0
+  }
+
+  //32 [Boolean to String Conversion](https://edabit.com/challenge/KSTkFSnaYBJdo6PHx)
+
+  function boolToString(flag){
+    return flag + "";
+  }
+
+  //33 [Using Arrow Functions](https://edabit.com/challenge/QkvDge63crdGnMfvM)
+
+// create your arrow function below
+arrowFunc = (str) => {
+	return str
+}
+
+  //34 [Frames Per Second](https://edabit.com/challenge/d9suvbchE2bnHNQuK)
+
+  function frames(minutes, fps) {
+    return minutes * fps *60
+  }
+
+  //35 [Miserable Parody of a Calculator](https://edabit.com/challenge/fh9i7k936rvfjnCYR)
+
+  function calculator(str) {
+    return eval(str);
+  }
+
+  //36 [Buggy Code (Part 4)](https://edabit.com/challenge/4SmqXvQtL6qRgzfha)
+
+  function greeting(name) {
+    if(name === "Matt"){
+			return "Hello, Matt!"
+		}else if(name === "Helen"){
+			return "Hello, Helen!"
+		}
+			return "Hello, my Love!"
+}
+
+  //37 [Two Makes Ten](https://edabit.com/challenge/5erCDJ8eJDrXkmwTK)
+
+  function makesTen(a,b){
+    if(a ===10||b === 10){
+      return true
+    } else if(a+b === 10){
+      return true
+    } else {
+      return false
+    }
+  }
+
+  //38 [Let's Fuel Up!](https://edabit.com/challenge/YMWDcSuYwYvve3HZj)
+
+  function calculateFuel(n) {
+    return n>10 ? n*10 : 100
+  }
+
+  //39 [Buggy Code (Part 2)](https://edabit.com/challenge/uE9AJ4sSrrpSASMpu)
+
+  function maxNum(n1,n2) {
+    if (n2 > n1){
+      return n2;
+    }else{
+    return n1;
+    }
+  }
+
+  //40 [Compare Strings by Count of Characters](https://edabit.com/challenge/yHGowWucg3k2kJdZ4)
+ 
+  function comp(str1, str2) {
+    return str1.length === str2.length;
+  }
+
+ //41 [Is the String Empty?](https://edabit.com/challenge/EzbfiquDoAc2Zc9FL)
+
+ function isEmpty(s) {
+	return s.length == 0;
+}
+
+//42 [Check if an Integer is Divisible By Five](https://edabit.com/challenge/iBQYbSHZGhpktLRdn)
+
+function divisibleByFive(n) {
+	return n % 5 === 0;
+}
+
+//43 [Multiple of 100](https://edabit.com/challenge/qMr6wYGr6NaXAPQGF)
+
+function divisible(num) {
+  if(num % 100 === 0){
+		return true
+	} else {
+		return false
+	}
+}
+
+//44 [Recursion: Length of a String](https://edabit.com/challenge/4MSbtYFBiRtxHEkY8)
+
+function length(str) {
+	return str.length 
+}
+
+//45 []()
+//46 []()
+//47 []()
